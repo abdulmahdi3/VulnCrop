1973169e00b13bba357d4fc1a521aca451653531
abdulmahdi husni <m17202160@s.uokerbala.edu.iq>
2023-11-29 00:04:41 +0300

    Flask run XSStrike tool and return output in file

--- VulnCrop.py
+++ VulnCrop.py
@@ -1,31 +1,36 @@
-from flask import Flask, render_template, request
 import subprocess
 import os
+from flask import Flask, render_template, request  
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 
-@app.route("/") 
+# Path to xsstrike script
+XSSTRIKE_PATH = os.path.join(app.root_path, "XSStrike", "xsstrike.py") 
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/run_scan", methods=["POST"])
-def run_scan():
-    target = request.form["target"]
-    
-    # List of scanning tools 
-    tools = ["nmap", "nikto", "wpscan"]
-    
-    # Run scans and save output
-    outputs = {}
-    for tool in tools:
-        output = subprocess.check_output([tool, target])
-        outfile = f"{tool}_output.txt"
-        with open(outfile, "wb") as f:
-            f.write(output)
-        outputs[tool] = outfile
-        
-    # Render results page  
-    return render_template("results.html", outputs=outputs)
+@app.route("/scan", methods=["POST"])  
+def scan():
+
+    target_url = request.form["target_url"]
+
+    try:
+        # Run XSStrike 
+        output = subprocess.check_output(["python3", XSSTRIKE_PATH, "-u", target_url, "--crawl"])  
+    except subprocess.CalledProcessError as e:
+        # Handle errors 
+        print(e.output)  
+        return "XSStrike failed"
+
+    # Save output to file
+    outfile = "xsstrike_output.txt"
+    with open(outfile, "wb") as f: 
+        f.write(output)
+
+    # Pass file path to template
+    return render_template("results.html", output_file=outfile)
 
 if __name__ == "__main__":
-    app.run(port=8000,debug=True)
+   app.run(debug=True, port=5000)
